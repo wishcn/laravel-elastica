d98d98d21a350b5feb5f612ced267f1c803851a2
wish <wish@baffedu.com>
2018-11-30 12:21:08 +0800

    .

--- config/elastica.php
+++ config/elastica.php
@@ -17,11 +17,11 @@ return [
 
             'hosts' => [
                 [
-                    'host'     => env('ELASTICSEARCH_HOST', 'localhost'),
-                    'port'     => env('ELASTICSEARCH_PORT', 9200),
-                    'scheme'   => env('ELASTICSEARCH_SCHEME', null),
-                    'username' => env('ELASTICSEARCH_USER', null),
-                    'password' => env('ELASTICSEARCH_PASS', null),
+                    'host'      => env('ELASTICSEARCH_HOST', 'localhost'),
+                    'port'      => env('ELASTICSEARCH_PORT', 9200),
+                    'transport' => env('ELASTICSEARCH_SCHEME', null),
+                    'username'  => env('ELASTICSEARCH_USER', null),
+                    'password'  => env('ELASTICSEARCH_PASS', null),
                 ],
             ],
 
